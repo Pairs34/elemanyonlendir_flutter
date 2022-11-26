1a9d2d9c027ded09d0e481494fa0e72f825990e1
Pairs34 <a.yldrm@outlook.com.tr>
2022-11-26 20:36:33 +0300

    finished

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\Flutter\elemanyonlendir_flutter"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Ali YILDIRIM\Desktop\Projects\elemanyonlendir_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=2.5.1"
