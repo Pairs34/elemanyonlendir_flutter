d191d5e9784b655ecc71acd6f55a21c945f5dcbc
Pairs34 <a.yldrm@outlook.com.tr>
2022-12-05 23:58:38 +0300

    fixed deployment and will be fix ios conf for apns

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\Flutter\elemanyonlendir_flutter"
+export "FLUTTER_ROOT=/Users/aliyildirim/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aliyildirim/Documents/GitHub/elemanyonlendir_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1.0.0"
+export "FLUTTER_BUILD_NAME=2.5.1"
+export "FLUTTER_BUILD_NUMBER=2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
